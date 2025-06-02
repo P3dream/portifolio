44f9d434fc7eaae634571b882764e7d80b07fc2b
P3dream <Pedropizzi23@Hotmail.com>
2025-06-02 14:23:35 -0300

    improving about me section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,15 +1,16 @@
 import { useTranslation } from "react-i18next";
 import Card from "../components/Card";
 import Recomendations from "../model/Recomendations";
+import Resume from "./Resume";
 
 const About = () => {
   const { t } = useTranslation();
+
 
   const recomendationList = t("recomendations", { returnObjects: true }) as Recomendations[];
 
   return (
     <>
-      {/* About Section */}
       <div className="space-y-6 px-4 sm:px-0">
         <div className="flex flex-col sm:flex-row items-center sm:items-start text-center sm:text-left gap-6 mb-6">
           <img
@@ -27,6 +28,30 @@ const About = () => {
         </div>
       </div>
 
+      {/* <div className="mx-auto px-4 sm:px-6 py-6 bg-slate-800 bg-opacity-70 rounded-xl shadow-lg backdrop-blur text-gray-100 max-w-screen-2xl">
+        <h2 className="text-2xl sm:text-3xl font-bold text-gray-100 mb-4">{t("projects_title")}</h2>
+        <p className="text-base sm:text-lg text-gray-300 mb-6">{t("projects_desc")}</p>
+
+        <div
+          className="grid gap-6"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))"
+          }}
+        >
+          {projectsList.map((project) => (
+            <Card
+              key={project.title}
+              title={project.title}
+              description={project.description}
+              image={project.image}
+              path={project.path}
+            />
+          ))}
+        </div>
+      </div> */}
+      
+      <br></br>
       <div className="mx-auto px-4 sm:px-6 py-6 bg-slate-800 bg-opacity-70 rounded-xl shadow-lg backdrop-blur text-gray-100 max-w-screen-2xl">
         <h2 className="text-2xl sm:text-3xl font-bold text-gray-100 mb-4">{t("recommendations_title")}</h2>
         <p className="text-base sm:text-lg text-gray-300 mb-6">{t("recommendations_desc")}</p>
@@ -49,6 +74,8 @@ const About = () => {
           ))}
         </div>
       </div>
+      <br></br>
+      <Resume></Resume>
     </>
   );
 };
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,13 +1,13 @@
 import { useTranslation } from "react-i18next";
 
 const Resume = () => {
-  const { t, i18n } = useTranslation(); // aqui está o i18n
-  const lang = i18n.language; // acessa o idioma atual
+  const { t, i18n } = useTranslation();
+  const lang = i18n.language;
 
   const fileName = `Pedro-Pizzi-${lang}.pdf`;
 
   return (
-    <div className="max-w-4xl mx-auto px-4 sm:px-6 py-6 bg-slate-800 bg-opacity-70 rounded-xl shadow-lg backdrop-blur text-gray-100">
+    <div className="mx-auto px-4 sm:px-6 py-6 bg-slate-800 bg-opacity-70 rounded-xl shadow-lg backdrop-blur text-gray-100 max-w-screen-2xl">
       <h2 className="text-2xl sm:text-3xl font-bold text-gray-100 mb-4">
         {t("resume_title")}
       </h2>
