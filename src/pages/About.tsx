9369f820b1893a379d482e32b9be422dccbf2720
P3dream <Pedropizzi23@Hotmail.com>
2025-06-02 14:35:34 -0300

    last changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -27,29 +27,6 @@ const About = () => {
           </div>
         </div>
       </div>
-
-      {/* <div className="mx-auto px-4 sm:px-6 py-6 bg-slate-800 bg-opacity-70 rounded-xl shadow-lg backdrop-blur text-gray-100 max-w-screen-2xl">
-        <h2 className="text-2xl sm:text-3xl font-bold text-gray-100 mb-4">{t("projects_title")}</h2>
-        <p className="text-base sm:text-lg text-gray-300 mb-6">{t("projects_desc")}</p>
-
-        <div
-          className="grid gap-6"
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))"
-          }}
-        >
-          {projectsList.map((project) => (
-            <Card
-              key={project.title}
-              title={project.title}
-              description={project.description}
-              image={project.image}
-              path={project.path}
-            />
-          ))}
-        </div>
-      </div> */}
       
       <br></br>
       <div className="mx-auto px-4 sm:px-6 py-6 bg-slate-800 bg-opacity-70 rounded-xl shadow-lg backdrop-blur text-gray-100 max-w-screen-2xl">
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,20 +8,26 @@ const Projects = () => {
   const projectList = t("projects", { returnObjects: true }) as Project[];
 
   return (
-    <div className="max-w-4xl mx-auto px-4 sm:px-6 py-6 bg-slate-800 bg-opacity-70 rounded-xl shadow-lg backdrop-blur text-gray-100">
+    <div className="mx-auto px-4 sm:px-6 py-6 bg-slate-800 bg-opacity-70 rounded-xl shadow-lg backdrop-blur text-gray-100 max-w-screen-2xl">
       <h2 className="text-2xl sm:text-3xl font-bold text-gray-100 mb-4">{t("projects_title")}</h2>
       <p className="text-base sm:text-lg text-gray-300 mb-6">{t("projects_desc")}</p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+      <div
+      className="grid gap-6"
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(280px, 1fr))"
+      }}
+      >
         {projectList.map((project) => (
-            <Card
-              key={project.title}
-              title={project.title}
-              description={project.description}
-              image={project.image}
-              path={project.path}
-            />
-          ))}
+          <Card
+            key={project.title}
+            title={project.title}
+            description={project.description}
+            image={project.image}
+            path={project.path}
+          />
+        ))}
       </div>
     </div>
   );
