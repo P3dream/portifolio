71b4365b7d264c6b4e727543b6b5e2831ddda339
P3dream <Pedropizzi23@Hotmail.com>
2025-08-28 18:21:28 -0300

    fixing cases where it was not working well in small cellphones

--- src/components/AnimatedContainer.tsx
+++ src/components/AnimatedContainer.tsx
@@ -37,7 +37,7 @@ const AnimatedContainer = ({
       variants={container}
       initial="hidden"
       whileInView="show"
-      viewport={{ once, amount: 0.1 }}
+      viewport={{ once }}
       className={className}
     >
       {Array.isArray(children)
