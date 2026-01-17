b87115dc801f7709c4bd9fdbccde3e9fc61e6d30
p3dream <pedropizzi23@hotmail.com>
2026-01-17 18:48:01 +0100

    changing photo name

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import photo from "/assets/photo.jpg";
+import photo from "/assets/profilePhoto.jpg";
 
 const AboutMe = () => {
   const { t } = useTranslation();
