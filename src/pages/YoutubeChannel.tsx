d95d8970f62495eb363def71d1f0e8b91293da1d
p3dream <pedropizzi23@hotmail.com>
2026-02-22 17:41:14 -0300

    organizing assets

--- src/pages/YoutubeChannel.tsx
+++ src/pages/YoutubeChannel.tsx
@@ -7,14 +7,14 @@ const blogPosts = [
     titleKey: "blog_post_1_title",
     descKey: "blog_post_1_desc",
     youtubeUrl: "https://youtu.be/zOOKUdEnuGY",
-    imageUrl: "/assets/thumb-nest.jpg",
+    imageUrl: "/assets/thumbnails/thumb-nest.jpg",
   },
   {
     id: 2,
     titleKey: "blog_post_2_title",
     descKey: "blog_post_2_desc",
     youtubeUrl: "https://youtu.be/DsYMY290uc4",
-    imageUrl: "/assets/nest-project2.jpg",
+    imageUrl: "/assets/thumbnails/nest-project2.jpg",
   },
 ];
 
