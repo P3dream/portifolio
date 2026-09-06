502f02e819f61a1d39aea639e9880f01162ba834
P3dream <Pedropizzi23@Hotmail.com>
2026-09-06 11:12:27 -0300

    improving experience section

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,10 +1,10 @@
 import { Code2, Database, Layers3, ServerCog } from "lucide-react";
 
 const techSections = {
-    Backend: ["Nest.js", "Node.js", "Microservices", ".NET", "RESTful APIs", "Python"],
-    Frontend: ["React.js", "Vue.js", "Angular", "TypeScript"],
-    DevOps: ["AWS", "Docker", "CI/CD", "Grafana", "Prometheus", "Cybersecurity","Certificates"],
-    Databases: ["PostgreSQL", "MySQL", "MongoDB","SQLite","SQL Server","Sap Hana"]
+    Backend: ["Nest.js", "Node.js", ".NET", "RESTful APIs", "Python"],
+    Frontend: ["React.js", "Vue.js", "TypeScript"],
+    DevOps: ["AWS", "Docker", "CI/CD", "Grafana", "Prometheus", "Cybersecurity"],
+    Databases: ["PostgreSQL", "MySQL", ,"SQLite","SQL Server","S3","AWS Athena"]
 };
 
 const sectionIcons = {
